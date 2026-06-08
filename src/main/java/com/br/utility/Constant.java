cf50629f622e82e412cb0e9b0e4f4935464276a1
Mahakit1 <70052260+Mahakit1@users.noreply.github.com>
2026-06-08 15:05:33 +0700

    fix constant

--- src/main/java/com/br/utility/Constant.java
+++ src/main/java/com/br/utility/Constant.java
@@ -1,38 +1,38 @@
 package com.br.utility;
 
+import java.io.InputStream;
+import java.util.Properties;
+
 public class Constant {
 
-	// Production
-//	 public static String PROFILENAME = "PRD"; // PRD
-//	 public static String MNELOGONURL = "https://bkrmvxm3.bangkokranch.com:21008/mne/servlet/MvxMCSvt"; // PRD
-//	 public static int APPPORT = 16105; // PRD
-//	 public static String APPSERVER = "192.200.9.190";
-//	 public static String DBNAME = "BRLDTA0100"; // PRD
-//	 public static String DBM3NAME = "M3FDBPRD"; // PRD
-//	 public static String WORKFLOW = "https://workflow.br-bangkokranch.com/webhook/service-request"; // PRD
+    public static final String PROFILENAME;
+    public static final String MNELOGONURL;
+    public static final int APPPORT;
+    public static final String APPSERVER;
+    public static final String DBNAME;
+    public static final String DBM3NAME;
+    public static final String WORKFLOW;
+    public static final String CHECKTOKENURL;
+    public static final String GENTOKENURL;
+    public static final String GENTOKENWOTOKENURL;
 
-	// Development
-	public static String PROFILENAME = "TST"; // TST
-	public static String MNELOGONURL = "https://bkrmvxm3.bangkokranch.com:22008/mne/servlet/MvxMCSvt"; // TST
-	public static int APPPORT = 16305; // TST
-	public static String APPSERVER = "192.200.9.190";
-	public static String DBNAME = "BRLDTABK01"; // TST
-	public static String DBM3NAME = "M3FDBTST"; // TST
-	public static String WORKFLOW = "https://workflow.br-bangkokranch.com/webhook/tst/service-request"; // TST
-
-	// Localhost
-	public static String CHECKTOKENURL = "http://localhost:8080/authen_api/auth/checktoken";
-	public static String GENTOKENURL = "http://localhost:8080/authen_api/auth/gentoken";
-	public static String GENTOKENWOTOKENURL = "http://localhost:8080/authen_api/auth/gentokenwotoken";
-	
-	// Server
-//	public static String CHECKTOKENURL = "http://192.200.9.186:8080/authen_api/auth/checktoken";
-//	public static String GENTOKENURL = "http://192.200.9.186:8080/authen_api/auth/gentoken";
-//	public static String GENTOKENWOTOKENURL = "http://192.200.9.186:8080/authen_api/auth/gentokenwotoken";
-	
-	// Domain
-//	public static String CHECKTOKENURL = "https://service.br-bangkokranch.com/authen_api/auth/v2/checktoken";
-//	public static String GENTOKENURL = "https://service.br-bangkokranch.com/authen_api/auth/v2/gentoken";
-//	public static String GENTOKENWOTOKENURL = "https://service.br-bangkokranch.com/authen_api/auth/v2/gentokenwotoken";
-
+    static {
+        Properties props = new Properties();
+        try (InputStream is = Constant.class.getClassLoader().getResourceAsStream("config.properties")) {
+            if (is == null) throw new RuntimeException("config.properties not found in classpath");
+            props.load(is);
+        } catch (Exception e) {
+            throw new ExceptionInInitializerError(e);
+        }
+        PROFILENAME = props.getProperty("PROFILENAME");
+        MNELOGONURL = props.getProperty("MNELOGONURL");
+        APPPORT = Integer.parseInt(props.getProperty("APPPORT"));
+        APPSERVER = props.getProperty("APPSERVER");
+        DBNAME = props.getProperty("DBNAME");
+        DBM3NAME = props.getProperty("DBM3NAME");
+        WORKFLOW = props.getProperty("WORKFLOW");
+        CHECKTOKENURL = props.getProperty("CHECKTOKENURL");
+        GENTOKENURL = props.getProperty("GENTOKENURL");
+        GENTOKENWOTOKENURL = props.getProperty("GENTOKENWOTOKENURL");
+    }
 }
